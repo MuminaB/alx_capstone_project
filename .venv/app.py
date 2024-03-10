6989186bee061dc2e07ddcd393a3d850cf9910a0
MuminaB <barrowmuminatou@gmail.com>
2024-03-10 13:06:16 +0000

    check

---
+++ .venv/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_emplate, request
+from flask_mail import Mail, Message
+import os
+
+app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
+
+mail = Mail(app)
+
+
